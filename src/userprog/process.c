01eda5b65f026bb7f9c4a3991158716970aafaa2
joamjoamjoam <trentcalllan@yahoo.com>
2015-02-19 15:13:05 -0800

    1

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -66,7 +66,7 @@ process_execute (const char *file_name)
         // push the argv array and argc variable onto the page file
         // using asm push and call
         printf("backwards order\n");
-        for (i = argc-1; i <= 0 ; i--) {
+        for (i = argc-1; i >= 0 ; i--) {
             printf("%s\n",argv[i]);
         }
         
